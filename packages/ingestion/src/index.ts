59ef63ae5559b3199782f2d5988699dc6076c319
Noahlieb <144489455+Noahlieb@users.noreply.github.com>
2026-08-16 21:13:24 -0400

    Add PhantomBuster live API client (launch/poll/fetch) so agents run automatically

--- packages/ingestion/src/index.ts
+++ packages/ingestion/src/index.ts
@@ -3,5 +3,6 @@ export * from "./ical.js";
 export * from "./rss.js";
 export * from "./jsonld.js";
 export * from "./phantombuster.js";
+export * from "./phantombuster-client.js";
 export * from "./manual.js";
 export * from "./registry.js";
